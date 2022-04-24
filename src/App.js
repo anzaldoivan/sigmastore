413fd4959f29e97ed83b25d3a462bd761dda4a7f
anzaldoivan <anzaldoivan98@gmail.com>
2022-04-23 23:54:36 -0300

    ItemListContainer added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/Home";
 import Navbar from "./components/NavBar";
+import ItemListContainer from "./components/container/ItemListContainer";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <ItemListContainer />
       <Routes>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
---
+++ src/components/container/ItemListContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ItemListContainer = (props) => {
+    // Incluye aquí el rendering de algún texto o título provisional que luego reemplazaremos por nuestro catálogo
+  }
+  
+
+export default ItemListContainer;
